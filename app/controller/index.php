e0cf62965bd874b45a6164d29bfe1d13de653c0b
Ryan Yonzon <rawswift@gmail.com>
2015-05-07 08:59:30 +0800

    Remove __construct method on index controller

--- app/controller/index.php
+++ app/controller/index.php
@@ -4,11 +4,6 @@ use Orinoco\Framework\View as View;
 
 class index
 {
-    public function __construct()
-    {
-        // code here are always executed
-    }
-
     public function index(View $view)
     {
     	return $view->render('index');
